9652c816740e2f1c5f23304face0b9c3635ec249
Zo Beki <uobeki.40@gmail.com>
2021-05-01 21:37:12 -0400

    components update

---
+++ employee-directory/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Employee Directory</h1>
+            <p>Click on a column headers to filter by heading or use the search bar to narrow results!</p>
+        </header>
+    );
+};
+
+export default Header;
